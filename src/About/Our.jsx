f4441bd898d45bb52ccb68e29d1197e1cfe9d69d
SouvikDasG <123242028+SouvikDasG@users.noreply.github.com>
2024-01-16 19:02:10 +0530

    Our Page is Made

--- src/About/Our.jsx
+++ src/About/Our.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import './Our.css'
 const Our = () => {
   return (
-    <>
-     <div>
-      <header>
-        <h1>Your Web Page Heading</h1>
-      </header>
-
-      <section>
-        <h2>About This Page</h2>
-        <p>
-          This is a simple web page with a heading and a description. You can use this as a starting point
-          for building your own web pages. Feel free to customize the HTML and CSS code to suit your needs.
-        </p>
-      </section>
-    </div>
-    </>
+    <div className='inner'>
+    <div className='page011'>
+<div className="pageO1">
+    <h1>Our Journey: <br/>
+PrecisionHub IT <br/>
+Industries Private <br/>
+Limited</h1>
+    <p>Founded in September 2023, <br/>
+PrecisionHub IT Industries Private <br/>
+Limited embarked on a <br/>
+transformative journey with a <br/>
+mission to redefine B2B IT solutions. <br/>
+From our humble beginnings, we <br/>
+have been dedicated to harnessing <br/>
+the power of cutting-edge <br/>
+technologies to drive innovation and <br/>
+efficiency for businesses across <br/>
+diverse industries.</p>
+</div>
+    </div></div>
   )
 }
 
