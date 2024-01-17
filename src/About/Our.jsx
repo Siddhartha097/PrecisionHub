e10295c870569da18fd2af49fb1069058174e68d
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-01-17 21:12:47 +0530

    Update Our.jsx

--- src/About/Our.jsx
+++ src/About/Our.jsx
@@ -39,74 +39,74 @@ const Our = () => {
     }, [])
 
     return (
-
-    
-    <div className='inner'>
-        <div className='page0 page1' style={{background: 'linear-gradient(to left ,  black 70%, gray)'}} ref={open1}>
-            <div className="page pages1"  >
-                <h1>
-                    Our Journey: PrecisionHub IT 
-                    <br/>
-                    Industries PrivateLimited
-                </h1>
-                <p>
-                    Founded in September 2023, 
-                    <br/>
-                    PrecisionHub IT Industries Private 
-                    <br/>
-                    Limited embarked on a 
-                    <br/>
-                    transformative journey with a 
-                    <br/>
-                    mission to redefine B2B IT solutions. 
-                    <br/>
-                    From our humble beginnings, we 
-                    <br/>
-                    have been dedicated to harnessing 
-                    <br/>
-                    the power of cutting-edge 
-                    <br/>
-                    technologies to drive innovation and 
-                    <br/>
-                    efficiency for businesses across 
-                    <br/>
-                    diverse industries.
-                </p>
+    <>
+        <div className='inner'>
+            <div className='page0 page1' style={{background: 'linear-gradient(to left ,  black 70%, gray)'}} ref={open1}>
+                <div className="page pages1"  >
+                    <h1>
+                        Our Journey: PrecisionHub IT 
+                        <br/>
+                        Industries PrivateLimited
+                    </h1>
+                    <p>
+                        Founded in September 2023, 
+                        <br/>
+                        PrecisionHub IT Industries Private 
+                        <br/>
+                        Limited embarked on a 
+                        <br/>
+                        transformative journey with a 
+                        <br/>
+                        mission to redefine B2B IT solutions. 
+                        <br/>
+                        From our humble beginnings, we 
+                        <br/>
+                        have been dedicated to harnessing 
+                        <br/>
+                        the power of cutting-edge 
+                        <br/>
+                        technologies to drive innovation and 
+                        <br/>
+                        efficiency for businesses across 
+                        <br/>
+                        diverse industries.
+                    </p>
+                </div>
             </div>
+
+            <button onClick={()=>{Explore(open1,open2)}} className='openBtn' type='button' ref={buttonOpen1}>
+                Open
+            </button>
+
+            <div className='page0 page2' style={{background: 'linear-gradient(to right ,  #da3535 60%, #f36b41fe)'}} ref={open2}>
+                <div className="page pages2" style={{ textAlign:'right'}}>
+                    <h1>
+                        Inception and Vision
+                    </h1>
+                    <br/>
+                    <p>
+                        Our journey began with a vision to 
+                        <br />
+                        elevate the standards of IT solutions, 
+                        <br />
+                        recognizing the pivotal role they play 
+                        <br />
+                        in shaping the future of businesses. 
+                        <br />
+                        From day one, we set out to create 
+                        <br />
+                        a company that blends expertise 
+                        <br />
+                        with creativity, delivering solutions 
+                        <br />
+                        that not only meet but exceed the 
+                        <br />
+                        evolving needs of our clients.
+                    </p>
+                </div>
+            </div>   
         </div>
-
-        <button onClick={()=>{Explore(open1,open2)}} className='openBtn' type='button' ref={buttonOpen1}>
-            Open
-        </button>
-
-        <div className='page0 page2' style={{background: 'linear-gradient(to right ,  #da3535 60%, #f36b41fe)'}} ref={open2}>
-            <div className="page pages2" style={{ textAlign:'right'}}>
-                <h1>
-                    Inception and Vision
-                </h1>
-                <br/>
-                <p>
-                    Our journey began with a vision to 
-                    <br />
-                    elevate the standards of IT solutions, 
-                    <br />
-                    recognizing the pivotal role they play 
-                    <br />
-                    in shaping the future of businesses. 
-                    <br />
-                    From day one, we set out to create 
-                    <br />
-                    a company that blends expertise 
-                    <br />
-                    with creativity, delivering solutions 
-                    <br />
-                    that not only meet but exceed the 
-                    <br />
-                    evolving needs of our clients.
-                </p>
-            </div>
-        </div>   
-    </div>
+    </>
     
   )
 }
