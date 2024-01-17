c458d7d28adc6039468b9011a49fddc68574c314
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-01-17 15:39:57 +0530

    gradiant added

--- src/About/Our.jsx
+++ src/About/Our.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useRef } from 'react'
 import './Our.css'
 
-
-
 const Our = () => {
 
     const open1 = useRef(null);
     const open2 = useRef(null);
 
-    function openn(op){
+    const openn = (op) => {
   
         op.current.classList.add("animate3")
         console.log(open1.classList)
     }
-    function opennRight(op){
+
+    const opennRight = (op) => {
   
         op.current.classList.add("animate2")
         console.log(open1.classList)
@@ -21,9 +20,9 @@ const Our = () => {
 
     useEffect(()=>{
         // openn(open1)
-        // openn(open2)
-        setTimeout(()=>{openn(open1)},1701)
-        setTimeout(()=>{opennRight(open2)},1700)
+        // opennRight(open2)
+        setTimeout(()=>{openn(open1)},1301)
+        setTimeout(()=>{opennRight(open2)},1300)
           
     }, [])
 
@@ -31,44 +30,70 @@ const Our = () => {
 
     
     <div className='inner'>
-    <div className='page0 page1' ref={open1}>
-<div className="page pages1"  >
-    <h1>Our Journey: 
-PrecisionHub IT <br/>
-Industries Private 
-Limited</h1>
-    <p>Founded in September 2023, <br/>
-PrecisionHub IT Industries Private <br/>
-Limited embarked on a <br/>
-transformative journey with a <br/>
-mission to redefine B2B IT solutions. <br/>
-From our humble beginnings, we <br/>
-have been dedicated to harnessing <br/>
-the power of cutting-edge <br/>
-technologies to drive innovation and <br/>
-efficiency for businesses across <br/>
-diverse industries.</p>
-</div>
+        <div className='page0 page1' style={{background: 'linear-gradient(to left ,  black 70%, gray)'}} ref={open1}>
+            <div className="page pages1"  >
+                <h1>
+                    Our Journey: PrecisionHub IT 
+                    <br/>
+                    Industries PrivateLimited
+                </h1>
+                <p>
+                    Founded in September 2023, 
+                    <br/>
+                    PrecisionHub IT Industries Private 
+                    <br/>
+                    Limited embarked on a 
+                    <br/>
+                    transformative journey with a 
+                    <br/>
+                    mission to redefine B2B IT solutions. 
+                    <br/>
+                    From our humble beginnings, we 
+                    <br/>
+                    have been dedicated to harnessing 
+                    <br/>
+                    the power of cutting-edge 
+                    <br/>
+                    technologies to drive innovation and 
+                    <br/>
+                    efficiency for businesses across 
+                    <br/>
+                    diverse industries.
+                </p>
+            </div>
+        </div>
 
-</div>
-    <div className='page0 page2' style={{backgroundColor:'#FF4B4B'}} ref={open2}>
-<div className="page pages2" style={{backgroundColor:'#FF7145', textAlign:'right'}}>
-    <h1>Inception and Vision</h1><br/>
-    <p>Our journey began with a vision to <br />
-elevate the standards of IT solutions, <br />
-recognizing the pivotal role they play <br />
-in shaping the future of businesses. <br />
-From day one, we set out to create <br />
-a company that blends expertise <br />
-with creativity, delivering solutions <br />
-that not only meet but exceed the <br />
-evolving needs of our clients.</p>
-</div>
+        {/* <button onClick={() => {}} className='openBtn' type='button'>
+            Open
+        </button> */}
 
-    </div>
-    
-    
-    
+        <div className='page0 page2' style={{background: 'linear-gradient(to right ,  #da3535 60%, #f36b41fe)'}} ref={open2}>
+            <div className="page pages2" style={{ textAlign:'right'}}>
+                <h1>
+                    Inception and Vision
+                </h1>
+                <br/>
+                <p>
+                    Our journey began with a vision to 
+                    <br />
+                    elevate the standards of IT solutions, 
+                    <br />
+                    recognizing the pivotal role they play 
+                    <br />
+                    in shaping the future of businesses. 
+                    <br />
+                    From day one, we set out to create 
+                    <br />
+                    a company that blends expertise 
+                    <br />
+                    with creativity, delivering solutions 
+                    <br />
+                    that not only meet but exceed the 
+                    <br />
+                    evolving needs of our clients.
+                </p>
+            </div>
+        </div>   
     </div>
     
   )
