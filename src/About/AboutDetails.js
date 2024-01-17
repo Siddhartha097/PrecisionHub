23602978f744b28922da99231719615d4edb72c3
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-01-18 00:14:00 +0530

    aboutdetails added

---
+++ src/About/AboutDetails.js
@@ -0,0 +1,183 @@
+const Page1 = [
+    {content: (
+        <>
+            <h1>
+                Our Journey: PrecisionHub IT 
+                <br/>
+                Industries PrivateLimited
+            </h1>
+            <p>
+                Founded in September 2023, 
+                <br/>
+                PrecisionHub IT Industries Private 
+                <br/>
+                Limited embarked on a 
+                <br/>
+                transformative journey with a 
+                <br/>
+                mission to redefine B2B IT solutions. 
+                <br/>
+                From our humble beginnings, we 
+                <br/>
+                have been dedicated to harnessing 
+                <br/>
+                the power of cutting-edge 
+                <br/>
+                technologies to drive innovation and 
+                <br/>
+                efficiency for businesses across 
+                <br/>
+                diverse industries.
+            </p>
+        </>
+    ), delay: 1301},
+    {content: (
+            <>
+                <h1>
+                    Our Journey: PrecisionHub IT 
+                    <br/>
+                    Industries PrivateLimited
+                </h1>
+                <p>
+                    Founded in September 2023, 
+                    <br/>
+                    PrecisionHub IT Industries Private 
+                    <br/>
+                    Limited embarked on a 
+                    <br/>
+                    transformative journey with a 
+                    <br/>
+                    mission to redefine B2B IT solutions. 
+                    <br/>
+                    From our humble beginnings, we 
+                    <br/>
+                    have been dedicated to harnessing 
+                    <br/>
+                    the power of cutting-edge 
+                    <br/>
+                    technologies to drive innovation and 
+                    <br/>
+                    efficiency for businesses across 
+                    <br/>
+                    diverse industries.
+                </p>
+            </>
+        ), delay: 1301},
+        {content: (
+            <>
+                <h1>
+                    Our Journey: PrecisionHub IT 
+                    <br/>
+                    Industries PrivateLimited
+                </h1>
+                <p>
+                    Founded in September 2023, 
+                    <br/>
+                    PrecisionHub IT Industries Private 
+                    <br/>
+                    Limited embarked on a 
+                    <br/>
+                    transformative journey with a 
+                    <br/>
+                    mission to redefine B2B IT solutions. 
+                    <br/>
+                    From our humble beginnings, we 
+                    <br/>
+                    have been dedicated to harnessing 
+                    <br/>
+                    the power of cutting-edge 
+                    <br/>
+                    technologies to drive innovation and 
+                    <br/>
+                    efficiency for businesses across 
+                    <br/>
+                    diverse industries.
+                </p>
+            </>
+        ), delay: 1301},
+
+]
+
+const Page2 = [
+    {content: (
+        <>
+            <h1>
+                Inception and Vision
+            </h1>
+            <br/>
+            <p>
+                Our journey began with a vision to 
+                <br />
+                elevate the standards of IT solutions, 
+                <br />
+                recognizing the pivotal role they play 
+                <br />
+                in shaping the future of businesses. 
+                <br />
+                From day one, we set out to create 
+                <br />
+                a company that blends expertise 
+                <br />
+                with creativity, delivering solutions 
+                <br />
+                that not only meet but exceed the 
+                <br />
+                evolving needs of our clients.
+            </p>
+        </>    
+    ), delay:1300},
+    {content: (
+        <>
+            <h1>
+                Inception and Vision
+            </h1>
+            <br/>
+            <p>
+                Our journey began with a vision to 
+                <br />
+                elevate the standards of IT solutions, 
+                <br />
+                recognizing the pivotal role they play 
+                <br />
+                in shaping the future of businesses. 
+                <br />
+                From day one, we set out to create 
+                <br />
+                a company that blends expertise 
+                <br />
+                with creativity, delivering solutions 
+                <br />
+                that not only meet but exceed the 
+                <br />
+                evolving needs of our clients.
+            </p>
+        </>    
+    ), delay:1300},
+    {content: (
+        <>
+            <h1>
+                Inception and Vision
+            </h1>
+            <br/>
+            <p>
+                Our journey began with a vision to 
+                <br />
+                elevate the standards of IT solutions, 
+                <br />
+                recognizing the pivotal role they play 
+                <br />
+                in shaping the future of businesses. 
+                <br />
+                From day one, we set out to create 
+                <br />
+                a company that blends expertise 
+                <br />
+                with creativity, delivering solutions 
+                <br />
+                that not only meet but exceed the 
+                <br />
+                evolving needs of our clients.
+            </p>
+        </>    
+    ), delay:1300},
+]
