9263af66f5bd8c68c5cd1c9f95603b25042b6183
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-01-29 15:35:37 +0530

    Card added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,9 +5,14 @@ import React from 'react';
 const Card = ({ image, content, title, align  }) => {
     return (
         <main className='card'>
-            <img src={image} alt='img' />
+            <img 
+                src={image} 
+                alt='img' 
+            />
             <div className='card_content' style={{ textAlign: align }}>
-                <h3>{title}</h3>
+                <h3>
+                    {title}
+                </h3>
                 <p>
                     {content}
                 </p>
