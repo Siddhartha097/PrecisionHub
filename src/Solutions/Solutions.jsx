bd1c1a82364c1fcf1923f2522a7b3a5096208002
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-02-23 13:25:23 +0530

    first commit

--- src/Solutions/Solutions.jsx
+++ src/Solutions/Solutions.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './Solutions.css';
 import sol3 from './sol3.PNG'
+import sol1 from './sol1.PNG'
+import sol2 from './sol2.PNG'
 
 const Solutions = () => {
     return (
         <main className='solutionsMain'>
             {/* One */}
             <div className='solutions_one'>
-                <h1 className='One_title'>New Software
+                {/* <h1 className='One_title'>New Software
                     <br />
                     Update
                     <br />
                     Status Report
                 </h1>
-                <h3 className='One_subtitle'>January 2025</h3>
+                <h3 className='One_subtitle'>January 2025</h3> */}
+                <img src={sol1} alt="" />
             </div>
 
             {/* Two */}
             <div className='solutions_two'>
-                <h1 className='Two_title'>Project Goals</h1>
+                {/* <h1 className='Two_title'>Project Goals</h1>
                 <div className='Two_briefs'>
                     <div className='brief_section'>
                         <h1 className='brief_title'>AUDIT THE CURRENT SOFTWARE PROGRAM</h1>
@@ -35,7 +38,8 @@ const Solutions = () => {
                         <p className='brief_desc'>Install and run a test on the redeveloped software</p>
                         <div className='end' />
                     </div>     
-                </div>
+                </div> */}
+                <img src={sol2} alt="" />
             </div>
 
             {/* Three */}
